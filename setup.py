4c0be59e8c93a43e5ee57d7fd083d17f5cdd16f3
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-11-22 16:06:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='CUADRADOSLATINOS',
       version='0.1',
       description='A Python package for something',
-      url='http://github.com/rvf0068/minimal',
-      author="Fernanda León,Eduardo Perez,Eduardo Alanis,Alejandro Sanchez"
-      author_email="ga351962@uaeh.edu.mx,sa374709@uaeh.edu.mx,pe439921@uaeh.edu.mx,al440184@uaeh.edu.mx"
+      url='https://github.com/fernandaleonn/CUADRADOSLATINOS',
+      author="Fernanda León,Eduardo Perez,Eduardo Alanis,Alejandro Sanchez",
+      author_email="ga351962@uaeh.edu.mx,sa374709@uaeh.edu.mx,pe439921@uaeh.edu.mx,al440184@uaeh.edu.mx",
       license='MIT',
-      packages=['minimal'],
+      packages=['CUADRADOSLATINOS'],
       zip_safe=False)
